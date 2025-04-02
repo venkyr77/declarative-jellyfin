0e706bed3e57d38429af291b7df9dc36b7fe5732
Snorre Ettrup Altschul <snorre@altschul.dk>
2025-04-03 01:27:23 +0200

    restructured options

--- modules/options/default.nix
+++ modules/options/default.nix
@@ -1,5 +1,11 @@
 {lib, ...}:
 with lib; {
+  imports = [
+    ./system.nix
+    ./encoding.nix
+    ./network.nix
+    ./branding.nix
+  ];
   options.services.declarative-jellyfin = {
     enable = mkEnableOption "Jellyfin Service";
 
