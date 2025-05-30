0129cba74da872793661776cd18545aac8e0e375
Sveske-Juice <carl.benjamin.dreyer@gmail.com>
2025-05-30 18:32:29 +0200

    WIP test for libraries, #19 is blocking

---
+++ tests/autorun/libraries.nix
@@ -0,0 +1,88 @@
+{pkgs ? import <nixpkgs> {}, ...}: let
+  name = "libraries";
+  port = 8096;
+in {
+  inherit name;
+  test = pkgs.nixosTest {
+    inherit name;
+    nodes = {
+      machine = {
+        config,
+        pkgs,
+        ...
+      }: {
+        imports = [
+          ../../modules/default.nix
+        ];
+
+        virtualisation.memorySize = 1024;
+
+        system.activationScripts.setupFolders =
+          /*
+          bash
+          */
+          ''
+            mkdir -p /data/Movies
+            mkdir -p /data/Shows
+            mkdir -p /data/Pictures
+            mkdir -p /data/Books
+            mkdir -p /data/Music
+          '';
+
+        services.declarative-jellyfin = {
+          network.PublicHttpPort = port;
+          enable = true;
+          system.IsStartupWizardCompleted = true;
+          openFirewall = true;
+          Users = {
+            admin = {
+              Password = "admin";
+              Permissions = {
+                IsAdministrator = true;
+              };
+            };
+          };
+          libraries = {
+            Movies = {
+              Enabled = true;
+              ContentType = "movies";
+              PathInfos = ["/data/Movies"];
+            };
+            Shows = {
+              Enabled = true;
+              ContentType = "tvshows";
+              PathInfos = ["/data/Shows"];
+            };
+            Photos = {
+              Enabled = true;
+              ContentType = "homevideos";
+              PathInfos = ["/data/Pictures"];
+            };
+            Books = {
+              Enabled = true;
+              ContentType = "books";
+              PathInfos = ["/data/Books"];
+            };
+            Music = {
+              Enabled = true;
+              ContentType = "music";
+              PathInfos = ["/data/Music"];
+            };
+          };
+        };
+      };
+    };
+
+    testScript =
+      /*
+      py
+      */
+      ''
+        machine.start()
+        machine.wait_until_succeeds("test -e /var/log/jellyfin-init-done", timeout=120)
+        machine.wait_until_succeeds("curl 127.0.0.1:8096", timeout=10)
+        machine.succeed("! journalctl --no-pager -b -u jellyfin.service | grep -v \"plugin\" | grep -q \"ERR\"")
+        # TODO: use api key to test that libraries are there
+      '';
+  };
+}
